4ec775de94dd2b1371103db8c6e6962c72399231
jihoonseo <seojh8910@gmail.com>
2024-04-01 13:55:06 +0900

    작정하고 장고 31강

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
+from profileapp.views import ProfileCreateView
 
 app_name = "profileapp"
 urlpatterns = [
-    path("hello_world/", hello_world, name='hello_world'),
+    path("create/", ProfileCreateView.as_view(), name='create'),
 ]
 
